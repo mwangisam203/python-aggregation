08aae167dee0dccc3dce87ddaab8717bdd149093
mwangisam203 <mwangisam203@gmail.com>
2026-02-05 01:23:11 -0500

    updt method

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 class Library:
     def __init__(self, name):
         self.name = name
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
         
 
 
@@ -21,3 +25,8 @@ book1 = Book("To Kill a Mockingbird", "Harper Lee")
 book2 = Book("Pride and Prejudice", "Jane Austen")
 book3 = Book("The Catcher in the Rye", "J. D. Salinger")
 book4 = Book("The Great Gatsby", "F. Scott Fitzgerald")
+
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+library.add_book(book4)
