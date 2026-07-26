6959749a6016535cf51d98213ba3c25bc5de60eb
mwangisam203 <mwangisam203@gmail.com>
2026-07-26 17:04:23 -0400

    #generator expression

--- aggrtest.py
+++ aggrtest.py
@@ -1,2 +1,6 @@
 scores = [85, 92, 78, 90]
 total = sum(scores)  # 345
+
+#generator expression
+orders = [{"amount": 50}, {"amount": 30}, {"amount": 20}]
+total = sum(o["amount"] for o in orders)  # 100
